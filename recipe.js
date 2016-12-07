7f3eb66e6ebc040d57cd9eb14b3af70d12f2a610
ldgarber <leah.garber88@gmail.com>
2016-12-06 22:04:21 -0500

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,24 @@
 'use strict';
+
+var addIngredient = function(recipe, ingredient, amount) {
+  recipe[ingredient] = amount; 
+  return recipe; 
+}
+
+var removeIngredient = function(recipe, ingredient) {
+  delete recipe[ingredient]; 
+  return recipe; 
+}
+
+var updateIngredient = function(recipe, ingredient, amount) {
+  recipe[ingredient] = amount; 
+  return recipe; 
+}
+
+var readRecipe = function(recipe) {
+  Object.keys(recipe).forEach(function(ingredient){
+    var amount = recipe[ingredient]; 
+    console.log("this recipe calls for " + amount + " of " + ingredient);   
+    return; 
+  })
+}
